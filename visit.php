db43c054506c280d07f8a8ab4e5f6d9a8d5830d8
rma7qb <rma7qb@labunix01.cs.virginia.edu>
2015-11-15 20:22:32 -0500

    adding separate views of table data

---
+++ visit.php
@@ -0,0 +1,32 @@
+<!DOCTYPEhtml>
+<html>
+<head>
+</head>
+<body>
+<h1>CS4750</h1>
+<?php
+
+$conn = new mysqli('stardock.cs.virginia.edu', 'cs4750igs3pw', 'fall2015','cs4750igs3pw');
+
+if ($conn->connect_error) {
+
+         die("Connection failed: " . $conn->connect_error);
+
+}
+else {
+	//diagnosis, diagnosis treatment, Patient, Patient Visit, 
+	//Physician, Physician visit, treatment, visit, visit diagnosis, visit treatment 
+	$sql = "SELECT * FROM Visit";
+        $result = $conn->query($sql);
+	echo "******************************";
+
+	while ($row = $result->fetch_assoc()):
+		foreach($row as $key=>$value):
+			echo "<p>$key => $value</p>";
+		endforeach;
+		echo "******************************";
+	endwhile;
+}
+?>
+</body>
+</html>
