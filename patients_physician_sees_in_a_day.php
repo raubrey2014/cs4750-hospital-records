ac652527e4b62c5132ed046364802e975a29fa26
raubrey2014 <rma7qb@virginia.edu>
2015-12-03 12:06:13 -0500

    Specialists on a day and patients seen

---
+++ patients_physician_sees_in_a_day.php
@@ -0,0 +1,82 @@
+<!DOCTYPEhtml>
+<html>
+        <head>
+                <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+                <script src="https://afarkas.github.io/webshim/js-webshim/minified/polyfiller.js"></script>
+                <link rel="stylesheet" type="text/css" href="hospital.css" />
+        </head>
+        <body>
+                <script>
+                        webshim.polyfill('forms forms-ext');
+                </script>
+<h1>Patients seen by a Physician on a Day</h1>
+
+<form>
+        <input type='hidden' name='SUBMIT' value='1' />
+        Date: <input type='date' name='DATE' value='<?php echo $_REQUEST['DATE'] ?>' />
+        Physician Name:
+        <?php
+        $sql = "SELECT `Physician ID`, Name FROM Physician";
+        $conn = new mysqli('stardock.cs.virginia.edu', 'cs4750igs3pw', 'fall2015','cs4750igs3pw');
+        if ($conn->connect_error) {
+
+         die("Connection failed: " . $conn->connect_error);
+
+        }
+        else{
+            $result = $conn->query($sql);
+            echo "<select name='PhysicianName'>";
+            while ($row = $result->fetch_assoc()) {
+                echo "<option value='" . $row['Physician ID'] . "'>" . $row['Name'] . "</option>";
+            }
+            echo "</select>";
+        }
+        ?>
+	<input type='submit' />
+</form>
+
+<?php
+
+
+$conn = new mysqli('stardock.cs.virginia.edu', 'cs4750igs3pw', 'fall2015','cs4750igs3pw');
+
+if ($conn->connect_error) {
+
+         die("Connection failed: " . $conn->connect_error);
+
+}
+else if ($_REQUEST['SUBMIT']) {
+        $day = $_REQUEST['DATE'];
+        $physician = $_REQUEST['PhysicianName'];
+        $sql = "SELECT * from Physician WHERE `Physician ID` = '$physician'";
+        $name = $conn->query($sql)->fetch_assoc()['Name'];
+        echo $day ." ".$name;
+        echo "<h1>Results for $name on $day</h1>";
+
+        echo "<table class='result-list' cellspacing='5px'>";
+        echo "<tr><th>Patient Visited</th></tr>";
+        $init = "SELECT `Visit ID`, `SSN` FROM `Physician Visit` NATURAL JOIN Visit NATURAL JOIN `Patient Visit` WHERE `Physician ID` = '$physician' AND Date = STR_TO_DATE('$day', '%Y-%m-%d')" ;
+        $result4 = $conn->query($init);
+        $count = 0;
+
+        while ($row = $result4->fetch_assoc()) {
+            $patient = $row['SSN'];
+            $patient_name = "SELECT Name FROM Patient WHERE SSN = $patient";
+            $name2 = $conn->query($patient_name)->fetch_assoc()['Name'];
+            echo "<tr><td>$name2</td></tr>";
+        }  
+        echo "</table>";
+        $init = "SELECT count(`Visit ID`) as initCount FROM `Physician Visit` NATURAL JOIN Visit WHERE `Physician ID` = '$physician' AND Date = STR_TO_DATE('$day', '%Y-%m-%d')" ;
+        $count = $conn->query($init)->fetch_assoc()['initCount'];
+        echo "<h3>$name saw a total of $count patients on $day</h3>";
+}
+
+
+?>
+<br />
+
+<a href='index.html'>
+        Back
+</a>
+</body>
+</html>
--- specialists_on_visits.php
+++ specialists_on_visits.php
@@ -31,18 +31,32 @@ if ($conn->connect_error) {
 }
 else if ($_REQUEST['SUBMIT']) {
 
-	specialization(1);
+	// specialization(1);
 
         //diagnosis, diagnosis treatment, Patient, Patient Visit, 
         //Physician, Physician visit, treatment, visit, visit diagnosis, visit treatment 
-	$x = $_REQUEST['PATIENT_SSN'];
-	$y = $_REQUEST['DATE'];
-        $sql = "SELECT `Visit ID` FROM Patient NATURAL JOIN `Patient Visit` NATURAL JOIN Visit WHERE SSN =`$x` AND  Date = STR_TO_DATE($y, '%Y-%m-%d')";
-        $result = $conn->query($sql);
+    	$x = $_REQUEST['PATIENT_SSN'];
+        echo $x;
+    	$y = $_REQUEST['DATE'];
+        echo $y;
+        // AND  Date = STR_TO_DATE($day, '%Y-%m-%d')"
+        // NATURAL JOIN Visit
+        //Patient NATURAL JOIN 
+        // WHERE SSN =`$x`
+        $init = "SELECT count(*) as initCount FROM `Patient Visit` NATURAL JOIN Patient NATURAL JOIN Visit WHERE SSN = $x AND Date = STR_TO_DATE('$y', '%Y-%m-%d')";
+        $result = $conn->query($init);
+        $number_of_visits = $result->fetch_assoc()['initCount'];
 
+        $sql = "SELECT `Visit ID` FROM `Patient Visit` NATURAL JOIN Patient NATURAL JOIN Visit WHERE SSN = $x AND Date = STR_TO_DATE('$y', '%Y-%m-%d')";
+        $result = $conn->query($sql);
+        echo "<h1>There were $number_of_visits visits on $y</h1>";
         while ($row = $result->fetch_assoc()) {
-		echo "<h1>asdf</h1>";
-		specialication($row['Visit ID']);
+
+            $visit_num = $row['Visit ID'];
+    		echo "<h2>Results from visit $visit_num</h2>";
+    		specialization($visit_num);
+            echo "<hr>";
+
         }
 }
 
@@ -56,17 +70,48 @@ function specialization($number){
 
         }
         else {
-	        echo "<table class='result-list' cellspacing='5px'>";
-        	echo "<tr><th>Name</th><th>Specialization</th>";
+            if ($row = $result2->fetch_assoc()){
+                ########################################
+                $sql3 = "SELECT Specialization, count(*) as SpecCount FROM Physician NATURAL JOIN `Physician Visit` WHERE `Visit ID` = '$number' GROUP BY Specialization";
+                $result3 = $conn2->query($sql3);
+                if ($conn2->connect_error) {
+                       die("Connection failed: " . $conn2->connect_error);
+
+                }
+                else {
+                    echo "<table class='result-list' cellspacing='5px'>";
+                    echo "<tr><th>Specialization</th><th>Number Attended</th></tr>";
+                    while ($row3 = $result3->fetch_assoc()):
+                        $spec = $row3['Specialization'];
+                        $specCount = $row3['SpecCount'];
+                        echo "<tr><td>$spec</td><td>$specCount</td></tr>";
+
+                    endwhile;
+                    echo "</table><br>";
+
+                }
+                ########################################
+                echo "<table class='result-list' cellspacing='5px'>";
+                echo "<tr><th>Name</th><th>Specialization</th>";
+                $name = $row['Name'];
+                $spec = $row['Specialization'];
+                echo "<tr><td>$name</td><td>$spec</td></tr>";
                 while ($row2 = $result2->fetch_assoc()):
-       		        echo "<tr>";
-                        foreach($row2 as $key2=>$value2):
-	                	echo "<td>" . $value2 . "</td>";
-                        endforeach;
-	                echo "</tr>";
+                    echo "<tr>";
+                    foreach($row2 as $key2=>$value2):
+                	   echo "<td>" . $value2 . "</td>";
+                    endforeach;
+                    echo "</tr>";
                 endwhile;
-		echo "</table>";
+                echo "</table>";
+            }
+            else{
+                echo "There were no Physicians involved in this visit.";
+            }
         }
+        
+
+
 }
 
 ?>
