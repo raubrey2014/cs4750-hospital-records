0dea6cb419332df1661b95cbd65f95b249148624
igs3pw <igs3pw@labunix01.cs.virginia.edu>
2015-12-06 15:37:42 -0500

    Unified treatment and add_treatment.

--- treatment.php
+++ treatment.php
@@ -3,8 +3,55 @@ include 'login.php';
 validate_creds();
 include 'header.html';
 ?>
-<div class='content'>
-<h1>View Treatments</h1>
+<h1>Add New Treatment</h1>
+
+<?php 
+
+if (isset($_REQUEST['Name'])){
+	create_treatment();
+}
+
+function create_treatment(){
+	$conn = new mysqli('stardock.cs.virginia.edu', 'cs4750igs3pw', 'fall2015','cs4750igs3pw');
+
+	if ($conn->connect_error) {
+
+        die("Connection failed: " . $conn->connect_error);
+
+	} else {
+		$treatment = $_REQUEST['Name'];
+		$description = $_REQUEST['Descr'];
+		
+		$sql = "INSERT INTO Treatment (`Treatment Name`, `Treatment Description`) VALUES ('$treatment', '$description')";
+		$result = $conn->query($sql);
+		if ($result) {
+			echo "<h3>Treatment definition successfully added.</h3>";
+		} else {
+			echo "<h2>Failed to add Treatment: " . $conn->error . "</h2>";
+		}
+	}
+	
+	$conn->close();
+}
+
+?>
+
+<div id="container">
+<form role="form" class='form-inline'>
+  <table>
+    <tr>
+     <td style='text-align: right'><label for="Name">Treatment Name:</label></td>
+     <td><input type="Name" class="form-control" id="Name" name="Name"></td>
+    </tr><tr>
+     <td style='text-align: right'><label for="Descr">Treatment Description:</label></td>
+     <td><textarea type="Name" class="form-control" id="Descr" name="Descr"></textarea></td>
+    </tr>
+  </table>
+  <input type="submit" class="btn btn-default" />
+</form>
+</div>
+
+<h1>List of Treatments</h1>
 
 <?php
 
@@ -32,7 +79,6 @@ else {
 }
 ?>
 </table>
-</div>
 
 <?php
 include 'footer.html';
